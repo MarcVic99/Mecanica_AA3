dde16ac5db0c5f625946855c166b71eb9e6bccff
Aniol <aniolbarnadat@gmail.com>
2022-04-19 17:49:39 +0200

    Edited verletIntegrator to use currentPos & has GetNewVelocity()

--- code/include/VerlettIntegrator.h
+++ code/include/VerlettIntegrator.h
@@ -8,6 +8,7 @@ public:
 	~VerlettIntegrator();
 
 	glm::vec3 GetAcceleration(glm::vec3 forceAcum, float mass);
-	glm::vec3 GetNewPosition(glm::vec3 lastPos, glm::vec3 accel, float dt);
+	glm::vec3 GetNewPosition(glm::vec3 currPos, glm::vec3 lastPos, glm::vec3 accel, float dt);
+	glm::vec3 GetNewVelocity(glm::vec3 currPos, glm::vec3 lastPos, float dt);
 };
 
--- code/src/VerlettIntegrator.cpp
+++ code/src/VerlettIntegrator.cpp
@@ -13,7 +13,12 @@ glm::vec3 VerlettIntegrator::GetAcceleration(glm::vec3 forceAcum, float mass)
 	return forceAcum / mass;
 }
 
-glm::vec3 VerlettIntegrator::GetNewPosition(glm::vec3 lastPos, glm::vec3 accel, float dt)
+glm::vec3 VerlettIntegrator::GetNewPosition(glm::vec3 currPos, glm::vec3 lastPos, glm::vec3 accel, float dt)
 {
-	return lastPos + accel * dt * dt;
+	return currPos + (currPos - lastPos) + accel * dt * dt;
+}
+
+glm::vec3 VerlettIntegrator::GetNewVelocity(glm::vec3 newPos, glm::vec3 currPos, float dt)
+{
+	return (newPos - currPos) / dt;
 }
